cacb3252137426bc5bf16016a6e611cbad1ebab4
James Fenn <18jafenn90@gmail.com>
2016-05-23 18:53:55 -0400

    FORGOT SOMETHING

--- app/src/main/java/pasta/streamer/utils/Settings.java
+++ app/src/main/java/pasta/streamer/utils/Settings.java
@@ -13,7 +13,7 @@ import pasta.streamer.R;
 
 public class Settings {
 
-    final public static String PRELOAD = "preload", LIMIT = "limit", THUMBNAILS = "thumbnails", DARK_THEME = "dark_theme", PALETTE = "palette", PRIMARY = "primary_color", ACCENT = "accent_color", QUALITY = "playback_quality", ORDER = "order";
+    final public static String PRELOAD = "preload", LIMIT = "limit", THUMBNAILS = "thumbnails", CARDS = "cards", DARK_THEME = "dark_theme", PALETTE = "palette", PRIMARY = "primary_color", ACCENT = "accent_color", QUALITY = "playback_quality", ORDER = "order";
     final public static int ORDER_ADDED = 0, ORDER_NAME = 1, ORDER_ARTIST = 2, ORDER_ALBUM = 3, ORDER_LENGTH = 4, ORDER_RANDOM = 5;
 
     public static boolean isPreload(Context context) {
@@ -26,6 +26,10 @@ public class Settings {
 
     public static boolean isThumbnails(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(THUMBNAILS, true);
+    }
+
+    public static boolean isCards(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(CARDS, false);
     }
 
     public static boolean isDarkTheme(Context context) {
@@ -77,5 +81,11 @@ public class Settings {
 
     public static int getTrackOrder(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context).getInt(ORDER, ORDER_ADDED);
+    }
+
+    public static int getColumnNumber(Context context, boolean landscape) {
+        int columns = 2;
+        if (landscape) columns++;
+        return columns;
     }
 }
